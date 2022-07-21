11f05032afd01d6d1ccaab1bed23a90cdc1b9ffc
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-07-21 17:43:40 -0600

    Added one more button

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 if(Cookies.get(`click_count`) === undefined){
     Cookies.set(`click_count`, 0);
 }
-
  
 function addToCount(details) {
     let click_count = parseInt(Cookies.get(`click_count`))  + 1;
@@ -10,3 +9,15 @@ function addToCount(details) {
 
 let button = document.querySelector(`button`);
 button.addEventListener(`click`, addToCount)
+
+if(Cookies.get(`click_count_two`) === undefined){
+    Cookies.set(`click_count_two`, 0);
+}
+ 
+function addToCountTwo(details) {
+    let click_count = parseInt(Cookies.get(`click_count_two`))  + 1;
+    Cookies.set(`click_count_two`, click_count)
+}
+
+let button_two = document.getElementById(`two`)
+button_two.addEventListener(`click`, addToCountTwo)
--- js/display.js
+++ js/display.js
@@ -1,4 +1,6 @@
-let count = Cookies.get(`click_count`);
 
 let timesClicked = Cookies.get(`click_count`);
-document.querySelector(`main`).insertAdjacentHTML(`afterend`, `<h1>The button was clicked ${timesClicked} times.</h1>`);
+document.querySelector(`main`).insertAdjacentHTML(`afterend`, `<h1>The button one was clicked ${timesClicked} times.</h1>`);
+
+let timesClickedTwo = Cookies.get(`click_count_two`);
+document.querySelector(`main`).insertAdjacentHTML(`afterend`, `<h1>The button two was clicked ${timesClickedTwo} times.</h1>`);
